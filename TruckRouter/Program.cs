bc8dee316cc246fe0a109d31beed5220c8be0b1d
Jeremy Beck <jerbeck@gmail.com>
2017-10-06 13:31:31 -0500

    Added hosting config file

--- TruckRouter/Program.cs
+++ TruckRouter/Program.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
-namespace TruckRouter
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+namespace TruckRouter {
+	public class Program {
+		public static void Main( string[] args ) {
+			BuildWebHost( args ).Run();
+		}
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-    }
+		public static IWebHost BuildWebHost( string[] args ) {
+			var config = new ConfigurationBuilder()
+				 .SetBasePath( Directory.GetCurrentDirectory() )
+				 .AddJsonFile( "hosting.json", optional: true )
+				 .Build();
+
+			return WebHost.CreateDefaultBuilder( args )
+				.UseKestrel()
+				.UseConfiguration(config)
+				.UseStartup<Startup>()
+				.Build();
+		}
+	}
 }
